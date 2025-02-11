60b4df14a54e2af4b4726dc7a72eaae8f3c03936
dhanushistiredd <149650212+dhanushistiredd@users.noreply.github.com>
2025-02-11 20:37:18 +0530

    Update Model.py

--- Model.py
+++ Model.py
@@ -13,8 +13,6 @@ y = data['credit_score']
 
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
-
-
 model = RandomForestRegressor(random_state=42)
 model.fit(X_train, y_train)
 
